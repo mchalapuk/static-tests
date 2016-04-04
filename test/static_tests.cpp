071d708df836a485dda31846bc23bcc585bbf297
Maciej Chałapuk <maciej@chalapuk.pl>
2016-04-04 04:15:12 +0200

    + build config (cmake)

---
+++ test/static_tests.cpp
@@ -0,0 +1,13 @@
+// licence: MIT
+// author: Maciej Chałapuk
+
+#include <iostream>
+
+int main() {
+  std::cerr << "This program does nothing. "
+    << "It's successful compilation means "
+    << "that all static tests passed."
+    << std::endl;
+  return 1;
+}
+
